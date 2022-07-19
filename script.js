ef4d13694f9896daa1a32ad4e7a4e67d3c16168d
IsaPhipho <isaphiladelpho@gmail.com>
2022-07-19 13:39:58 +0100

    Defining directions and ensuring the snake stays on the box

--- script.js
+++ script.js
@@ -22,17 +22,38 @@ function createSnake() {
   }
 }
 
+document.addEventListener('keydown', update);
+
+function update(event) {
+  if(event.keyCode == 37 && direction != 'right') direction = 'left';
+  if(event.keyCode == 38 && direction != 'down') direction = 'up';
+  if(event.keyCode == 39 && direction != 'left') direction = 'right';
+  if(event.keyCode == 40 && direction != 'up') direction = 'down';
+}
+
 function startGame() {
+  if(snake[0].x > 15 * box && direction == "right") snake[0].x = 0;
+  if(snake[0].x < 0 && direction == 'left') snake[0].x = 16 * box;
+  if(snake[0].y > 15 * box && direction == "down") snake[0].y = 0;
+  if(snake[0].y < 0 && direction == 'up') snake[0].y = 16 * box;
+  
+  for(i = 1; i < snake.length; i++) {
+      if(snake[0].x == snake[i].x && snake[0].y == snake[i].y) {
+          clearInterval(jogo);
+          alert('Game Over :(');
+      }
+  }
+
   createBG();
   createSnake();
 
   let snakeX = snake[0].x;
   let snakeY = snake[0].y;
 
-  if (direction == "right") snakeX += box;
-  if (direction == "left") snakeX -= box;
-  if (direction == "up") snakeY -= box;
-  if (direction == "down")  snakeY += box;
+  if(direction == "right") snakeX += box;
+  if(direction == "left") snakeX -= box;
+  if(direction == "up") snakeY -= box;
+  if(direction == "down")  snakeY += box;
 
   snake.pop();
 
